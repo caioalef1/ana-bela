2ec10ebd8759fa68f5189bf1d36c17f5581d033d
caioalef1 <65835881+caioalef1@users.noreply.github.com>
2022-01-26 12:58:09 -0300

    D1
    Dia #1

---
+++ index.php
@@ -0,0 +1,31 @@
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ESTILO CSS</title>
+    <link rel="stylesheet" href="style/style.css">
+
+   
+</head>
+<body>
+    <div id="cab">
+        <img src="img/amazon.png">
+        <ul>
+            <li>Home</li>
+            <li>Produtos</li>
+            <li>Contatos</li>
+        </ul>
+    </div>
+    <div id="corp">
+        <div class="prod">Produto 1</div>
+        <div class="prod">Produto 2</div>
+        <div class="prod">Produto 3</div>
+    </div>
+    <div id="rodape">
+        All Right Reserved ©
+    </div>
+</body>
+</html>
+
+
