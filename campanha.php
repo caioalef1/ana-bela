84bf4ef4c347bdc7921008c83500cf60b74d47a7
caioalef1 <65835881+caioalef1@users.noreply.github.com>
2022-01-27 11:42:12 -0300

    site fictício feito com HTML+CSS
    Projeto solicitado no curso da Udemy utilizando somente HTML + CSS

--- campanha.php
+++ campanha.php
@@ -43,7 +43,7 @@
     The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
         </p>
         <div id="rodape">
-        <h4>Todos os direitos reservados ©</h4>
+            <h4>Todos os direitos reservados ©</h4>
         </div>
     </div>
     
--- contato.php
+++ contato.php
@@ -9,20 +9,36 @@
 </head>
 <body>
     <div id="principal">
-        <div id="conteudo">
-            <form>
-                <label for="fname">Primeiro Nome:</label><br>
+        <div id='menu'>
+                <ul>
+                    <a href="index.php">Home |</a>
+                    <a href="biografia.php">BIOGRAFIA |</a>
+                    <a href="campanha.php">CAMPANHAS PUBLICITÁRIAS |</a>
+                    <a href="contato.php">CONTATOS</a>
+                </ul>
+            </div>
+        <div id="area_form">
+            <div class="form">
+                <label for="fname">Como gostaria de ser chamado</label><br>
                 <input type="text" id="fname" name="fname"><br>
-                <label for="lname">Ultimo nome:</label><br>
-                <input type="text" id="lname" name="lname">
+            </div>
+            <div class="form">
                 <label for="cellphone">Numero de contato</label>
                 <input type="number" id="cellphone" name="cellphone">
+            </div>
+            <div class="form">
                 <label for="email">Email</label>
                 <input type="email" id="email" name="email">
+            </div>
+            <div class="form">
                 <label for="mensagem">Mensagem</label>
                 <textarea name="mensagem" id="mensagem" cols="30" rows="10"></textarea>
+            </div>
                 <button>Enviar</button>
             </form>
+        </div>
+        <div id="rodape">
+            <h4>Todos os direitos reservados ©</h4>
         </div>
     </div>
 </body>
